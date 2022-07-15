365cb8a0903bdc6105e3532aa522f65b6a3bb94b
Polina Raguilo <palina.rahila@itechart-group.com>
2022-07-15 17:02:34 +0300

    feat: add main components

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import { IFooterProps } from "./Footer.props";
+import cn from "classnames";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
-const Footer = ({ ...props }: IFooterProps) => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: IFooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,17 +3,16 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { ILayoutProps } from "./Layout.props";
 import Sidebar from "./Sidebar";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
